cb9ce4a3f7ad605cba1f5b0e469084fac7855245
Cenk SOYKAN <cenksoykan@cenksoykan.com>
2018-04-14 01:11:36 +0300

    Make prediction script

---
+++ scripts/pred.py
@@ -0,0 +1,53 @@
+"""
+========================
+    Face recognition
+========================
+
+Summary:
+    Face recognition using harrcascade and SVM
+
+To Run:
+    * To run it specify that the file you want to predict result
+        python pred.py somePicture.pgm
+
+"""
+
+from os import path
+import sys
+import cv2
+from svm import predict
+
+print(__doc__)
+
+FACE_DIM = (50, 50)  # h = 50, w = 50
+DISPLAY_FACE_DIM = (200, 200)
+FRONTALFACE = path.join(
+    path.dirname(cv2.__file__), "data", "haarcascade_frontalface_default.xml")
+PROFILEFACE = path.join(
+    path.dirname(cv2.__file__), "data", "haarcascade_profileface.xml")
+FACE_CASCADE = cv2.CascadeClassifier(FRONTALFACE)
+SIDEFACE_CASCADE = cv2.CascadeClassifier(PROFILEFACE)
+
+if len(sys.argv) == 1:
+    print("\nError: No Any Face Picture Specified\n")
+    exit()
+elif len(sys.argv) > 2:
+    print("\nError: More Than One Face Picture Specified\n")
+    exit()
+else:
+    DATA_PATH = sys.argv[1]
+
+if not path.exists(DATA_PATH):
+    print("\nError: There is no picture in this direction\n")
+    exit()
+
+if DATA_PATH.endswith(".png") or DATA_PATH.endswith(
+        ".jpg") or DATA_PATH.endswith(".jpeg") or DATA_PATH.endswith(".pgm"):
+    FACE = cv2.imread(DATA_PATH, 0)
+else:
+    print(
+        "\nError: File extension has to be one of these: png, jpg, jpeg, pgm\n"
+    )
+    exit()
+
+print("This is picture of", "\"" + predict(FACE) + "\"")
